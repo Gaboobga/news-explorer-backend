663b9e82a56bcf69854930a726049a83dde1f708
Gaboobga <gabooobgaa@gmail.com>
2026-05-16 19:51:42 -0600

    feat: add CORS middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -11,6 +12,7 @@ const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://localhost:27017/news-explorer');
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger);
